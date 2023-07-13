38fc1c269f210b789db0261557d31c6ac95de212
Bruno Weber <brunogw9@gmail.com>
2023-07-13 19:28:02 +1000

    Cookie "working"
    Just need to fix a details where 1 cookie is written as "cookie" not "cookies"

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 function reload() {
     location.reload()
 }
-window.setTimeout(reload, 1000);
+window.setTimeout(reload, 3000);
+
+cookie_balance_text = 0
+
+function bake_cookie() {
+    cookie_balance_text ++
+    document.getElementById("cookie_balance").innerHTML = cookie_balance_text + " Cookies";
+}
+
+if (cookie_balance_text = 1) {
+    document.getElementById("cookie_balance").innerHTML = cookie_balance_text + " Cookie";
+}
