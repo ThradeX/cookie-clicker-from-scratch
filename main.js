4baed674a7e68d29ac68107d632144f3cc86e248
Bruno Weber <brunogw9@gmail.com>
2023-07-23 17:14:59 +1000

    Layout skeleton done.

--- main.js
+++ main.js
@@ -9,3 +9,12 @@ function bake_cookie() {
         document.getElementById("cookie_balance").innerHTML = cookie_balance_text + " Cookies";
     }
 }
+
+function update_title() {
+    if (cookie_balance_text == 1) {
+        document.title = cookie_balance_text + " Cookie" + " - Cookie Clicker";
+    } else {
+        document.title = cookie_balance_text + " Cookies" + " - Cookie Clicker";
+    }
+}
+setInterval(update_title, 2000);
