f7757dc04a60d47b26b5199cc786907727bdaf77
Libanori <69210151+LibanoriJS@users.noreply.github.com>
2023-08-03 22:45:58 +1000

    fixing JS & reseting CSS

--- main.js
+++ main.js
@@ -1,7 +1,28 @@
+var cookie_balance = 0
 var cookie_balance_text = 0
+var observers = []
+
+const click = new Audio("snd/click1.mp3")
+
+function addObserver(observer) {
+    observers.push(observer);
+}
+function notifyObservers() {
+    observers.forEach(function(observer) {
+      observer(cookie_balance_text);
+    });
+}
+
+setInterval(function() {
+    notifyObservers();
+}, 1000);
+
 
 function bake_cookie() {
-    cookie_balance_text ++
+    cookie_balance_text = cookie_balance_text + 1
+    cookie_balance = cookie_balance + 1
+
+    click.play();
 
     if (cookie_balance_text == 1) {
         document.getElementById("cookie_balance").innerHTML = cookie_balance_text + " cookie";
@@ -18,3 +39,10 @@ function update_title() {
     }
 }
 setInterval(update_title, 2000);
+
+
+function unlock_cursor () {
+    if (cookie_balance >= 1) {
+        document.getElementById("buttom").style.opacity = "100%";
+    }
+}
